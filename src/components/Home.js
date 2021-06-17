fafe371a5ab9f1f507280b247b35d7d6822a83e2
Sean Bogolin <sean.bogolin@gmail.com>
2021-06-17 13:20:15 -0500

    Tutorials 6 & 7 completed

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,19 @@
 import React from 'react'
 
 const Home = () => {
+	const handleClick = (e) => {
+		console.table(`hello`, e)
+	}
+
+	const handleClickParams = (name) => {
+		console.log(`hello, ${name}`)
+	}
+
 	return (
 		<div className='home'>
 			<h2>Homepage</h2>
+			<button onClick={handleClick}>Click</button>
+			<button onClick={() => handleClickParams('Sean')}>Click Params</button>
 		</div>
 	)
 }
