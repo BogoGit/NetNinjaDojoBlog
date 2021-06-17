5442fac0cb7c9c5884fa33f4268716a1833ac7f7
Sean Bogolin <sean.bogolin@gmail.com>
2021-06-17 13:27:58 -0500

    Tutorial 8 complete. Content CSS update and useState test.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,19 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Home = () => {
+	const [name, setName] = useState('Sean')
+
 	const handleClick = (e) => {
-		console.table(`hello`, e)
+		setName('Bogo')
 	}
 
-	const handleClickParams = (name) => {
-		console.log(`hello, ${name}`)
-	}
+	// const handleClickParams = (name) => {
+	// 	console.log(`hello, ${name}`)
+	// }
 
 	return (
 		<div className='home'>
-			<h2>Homepage</h2>
+			<h2>Homepage for {name}</h2>
 			<button onClick={handleClick}>Click</button>
-			<button onClick={() => handleClickParams('Sean')}>Click Params</button>
+			{/* <button onClick={() => handleClickParams({ name })}>Click Params</button> */}
 		</div>
 	)
 }
